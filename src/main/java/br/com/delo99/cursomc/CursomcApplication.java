543039de8649a5b1518f8ab183272f510577f0de
Joao Carlos Moura <jmoura@delo99.com.br>
2018-07-02 20:35:24 -0300

    Criando operacao de instanciação

--- src/main/java/br/com/delo99/cursomc/CursomcApplication.java
+++ src/main/java/br/com/delo99/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package br.com.delo99.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.delo99.cursomc.domain.Categoria;
+import br.com.delo99.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	
+	// Apenas como teste
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
 	}
 }
